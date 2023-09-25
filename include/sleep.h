4557c46760b1582e88d0b35483b57ea2c61e76ad
LeinneSW <nno88551@naver.com>
2023-09-25 11:12:17 +0900

    불필요한 슬립 기능 제거

--- include/sleep.h
+++ include/sleep.h
@@ -1,19 +1,5 @@
 #include <esp_sleep.h>
 
-void lightSleep(int64_t us = -1){
-    if(us > 0 && esp_sleep_enable_timer_wakeup(us) != ESP_OK){
-        printf("Invalid sleep time. [value: %d]\n", us);
-    }
-    esp_light_sleep_start();
-}
-
-void deepSleep(int64_t us = -1){
-    if(us > 0 && esp_sleep_enable_timer_wakeup(us) != ESP_OK){
-        printf("Invalid sleep time. [value: %d]\n", us);
-    }
-    esp_deep_sleep_start();
-}
-
 void hibernate(){
     esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_OFF);
 
