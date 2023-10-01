7e2c1eedf4377b8fcc445fcc6cbc0106822740e3
LeinneSW <nno88551@naver.com>
2023-10-02 03:33:17 +0900

    슬립기능 유틸에 통합

--- include/utils.h
+++ include/utils.h
@@ -8,8 +8,6 @@
 
 using namespace std;
 
-//#define DEBUG_MODE
-
 #ifdef DEBUG_MODE
 #define debug(fmt, args...) printf(fmt, ##args)
 #else
@@ -77,4 +75,31 @@ inline string urlDecode(const string& encoded){
         }
     }
     return decoded.str();
+}
+
+void hibernate(){
+    esp_sleep_pd_config(ESP_PD_DOMAIN_XTAL, ESP_PD_OPTION_OFF);
+
+    #if SOC_PM_SUPPORT_RTC_PERIPH_PD
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_OFF);
+    #endif
+
+    #if SOC_PM_SUPPORT_RTC_SLOW_MEM_PD
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
+    #endif
+
+    #if SOC_PM_SUPPORT_RTC_FAST_MEM_PD
+    esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_FAST_MEM, ESP_PD_OPTION_OFF);
+    #endif
+    esp_deep_sleep_start();
+}
+
+void deepSleep(gpio_num_t pin, int level, uint64_t time = 0){
+    debug("[SLEEP] Start Deep Sleep\n");
+    rtc_gpio_pullup_en(pin);
+    esp_sleep_enable_ext0_wakeup(pin, level);
+    if(time > 0){
+        esp_sleep_enable_timer_wakeup(time);
+    }
+    esp_deep_sleep_start();
 }
